868af35b6918e4f93c9707566b881b13bdc4ea8f
Sourav Patnaik <Sourav.Patnaik@ibm.com>
2022-07-16 14:12:05 +0530

    added doRetryableHTTPRequest
    added doRetryableHTTPRequest logic

--- client.go
+++ client.go
@@ -5,30 +5,55 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
+
+	"gopkg.in/retry.v1"
 )
 
-// Default avengers-backend URL
-const HostURL string = "http://localhost:8000"
+const (
+	HostURL              = "http://localhost:8000" // Default avengers-backend URL
+	DEFAULT_HTTP_TIMEOUT = 10 * time.Second
+	InitialBackoffDelay  = 1 * time.Second
+	MaxBackoffDelay      = 16 * time.Second // MaxBackoffDelay is a DelayType which increases delay between consecutive retries
+	MaxRetries           = 5
+)
 
 type Client struct {
 	HostURL    string
 	HTTPClient *http.Client
+	clock      retry.Clock
+	strategy   retry.Strategy
 }
 
-func NewClient(host *string) (*Client, error) {
+var Strategy = retry.LimitCount(MaxRetries, retry.Exponential{
+	Initial:  InitialBackoffDelay,
+	Factor:   2,
+	MaxDelay: MaxBackoffDelay,
+})
+
+func NewClient(host *string, clock retry.Clock, strategy retry.Strategy) (*Client, error) {
 	c := Client{
-		HTTPClient: &http.Client{Timeout: 10 * time.Second},
+		HTTPClient: &http.Client{Timeout: DEFAULT_HTTP_TIMEOUT},
 		HostURL:    HostURL,
 	}
 	if host != nil {
 		c.HostURL = *host
+	}
+	if clock != nil {
+		c.clock = clock
+	} else {
+		c.clock = nil
+	}
+	if strategy != nil {
+		c.strategy = strategy
+	} else {
+		c.strategy = Strategy
 	}
 	return &c, nil
 }
 
 //DoRequest will actually make a call to avengers-backend
 func (c *Client) DoRequest(req *http.Request) ([]byte, error) {
-	res, err := c.HTTPClient.Do(req)
+	res, err := c.doRetryableHTTPRequest(req)
 	if err != nil {
 		return nil, err
 	}
@@ -44,4 +69,21 @@ func (c *Client) DoRequest(req *http.Request) ([]byte, error) {
 	}
 
 	return body, err
+}
+
+func (c *Client) doRetryableHTTPRequest(req *http.Request) (resp *http.Response, err error) {
+	for a := retry.Start(Strategy, nil); a.Next(); {
+		resp, err = c.HTTPClient.Do(req)
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
+
+		if a.More() && resp.StatusCode/100 == 5 {
+			fmt.Printf("Attemtping to retry request, to %s", resp.Request.URL)
+			continue
+		}
+		return
+	}
+	return nil, fmt.Errorf("Retry strategy did not allow any attempts")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sourav977/avengers-client
 
 go 1.16
+
+require gopkg.in/retry.v1 v1.0.3
